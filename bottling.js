f9eea945e9e8c61854b9d66433422e7c127f7ae6
darren-chaney <75745239+darren-chaney@users.noreply.github.com>
2025-12-22 14:57:19 -0600

    Update bottling.js

--- bottling.js
+++ bottling.js
@@ -26,11 +26,6 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
 
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const auth = getAuth(app);
-
 /* ===============================
    State
    =============================== */
